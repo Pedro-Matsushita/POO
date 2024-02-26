07b1e116a68cc714b566704b83afec846528ee0e
Pedro Matsushita <137213776+Pedro-Matsushita@users.noreply.github.com>
2024-02-26 10:32:25 +0000

    Questão ProdutoEstoque adicionado

---
+++ Lista2/ControleDeEstoques/ProdutoEstoque.java
@@ -0,0 +1,18 @@
+public class ProdutoEstoque{
+    public static void main(String[] args) {
+        String nome;
+        int quantidade;
+        double precoUnitario;
+
+        void adicionarEstoque(int quantidade){
+
+        }
+        void removerEstoque(int quantidade){
+
+        }
+        void atualizarPreco(double novoPreco){
+
+        }
+        
+    }
+}
